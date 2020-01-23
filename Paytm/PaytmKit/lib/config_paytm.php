4c016cc2ed8ba3b94f50edeec62235095870482e
seepana sai kiran <saikiranseepana@gmail.com>
2020-01-24 03:14:40 +0530

    production keys removed

--- Paytm/PaytmKit/lib/config_paytm.php
+++ Paytm/PaytmKit/lib/config_paytm.php
@@ -8,8 +8,8 @@
 */
 
 define('PAYTM_ENVIRONMENT', 'PROD'); // PROD
-define('PAYTM_MERCHANT_KEY', 'Hb@fXyJ18rGEp94k'); //Change this constant's value with Merchant key received from Paytm.
-define('PAYTM_MERCHANT_MID', 'EzfMKn40833132020367'); //Change this constant's value with MID (Merchant ID) received from Paytm.
+define('PAYTM_MERCHANT_KEY', '');
+define('PAYTM_MERCHANT_MID', '');
 define('PAYTM_MERCHANT_WEBSITE', 'WEBSTAGING'); //Change this constant's value with Website name received from Paytm.
 
 $PAYTM_STATUS_QUERY_NEW_URL='https://securegw-stage.paytm.in/merchant-status/getTxnStatus';
