ad76a3b84992b76eb9ee822435cd482d156d60b7
saikiran-seepana <saiphanivittalkovvada@gmail.com>
2020-01-29 17:01:12 +0530

    The final directory structure

--- php/register/teamRegistrations.php
+++ php/register/teamRegistrations.php
@@ -9,10 +9,9 @@
 		<meta charset="utf-8">
 		<meta name="author" content="saikiranseepana">
 	    <meta name="viewport" content="width=device-width,initial-scale=1.0" charset="utf-8">
-	    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+	    <link rel="stylesheet" href="../../css/bootstrap/bootstrap.min.css">
 	    <link rel="stylesheet" type="text/css" href="../../css/workshopcs.css"/>
 	    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
-	    <link rel="stylesheet" href="../../css/carousel_ext.css">
 	    <link rel="stylesheet" href="../../css/navbar.css">
 	    <link rel="stylesheet" href="../../css/card.css">
 	</head>
@@ -51,8 +50,7 @@
 					';
 		        }
 		        else{
-					echo '<li class="nav-link"><a href="./register/login.php">Register</a></li>
-						';
+					echo '<li class="nav-link"><a href="./register/login.php">Register</a></li>';
 		        }
 		    ?>
   </header>
@@ -104,73 +102,65 @@
 			</div>
 		</form>
 	</div>
-	<footer  style="background-color: #2c292f;padding-top: 30px;min-height: 280;">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-3 text-center ">
-            <h4 class="my-3 text-white">About<br><span class="mx-auto my-3  text-warning ">MVGR College of Engineering</span></h4>
-            <ul style="color:aliceblue">
-              <li>Alumini</li>
-              <li>Events</li>
-              <li>News & Events</li>
-              <li>Gallery</li>
-              <li>Press & Media</li>
-            </ul>
-          </div>
-          <div class="col-md-3">
-            <div class="py-5">
-              <iframe width="230" height="230" frameborder="2" style="border:0;width:100%;height:auto;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3793.245677096944!2d83.40339111442094!3d18.06017798767875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a3be4c4081c8dbb%3A0x1c634bbcd67ea3bf!2sMaharaj%20Vijayaram%20Gajapathi%20Raj%20College%20of%20Engineering%20(Autonomous) !5e0!3m2!1sen!2sin!4v1577787304518!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+	<footer  style="background-color: #2c292f;padding-top: 30px;min-height: 100px; font-family: 'Righteous', cursive;">
+        <div class="container" >
+          <div class="row">
+                   <div class="col-md-4 text-center text-md-left">
+              <h4 class="my-3 text-warning">Quicklinks</h4>
+              <p class="footer-links font-weight-bold">
+                <a class="text" style="color:#50d8af;" href="../../">Home |</a>
+                <a class="text"style="color: #50d8af;" href="../technical/">Technical |</a>
+                <a class="text" style="color:#50d8af;" href="../workshop/">Workshops |</a>
+                <a class="text"style="color: #50d8af;" href="../cultural/">Culturals |</a>
+                <a class="text"style="color: #50d8af;" href="../login/">Register |</a>
+                <a class="text" style="color:#50d8af;" href="../about/">About</a>
+              </p>
+              <div class="py-1">
+                <h4 class="my-3 text-warning">Follow us on</h4>
+                <a target="_blank" href="https://www.facebook.com/aadhrita.mvgr"><i class="fab fa-facebook fa-2x text-primary mx-3"></i></a>
+                <a target="_blank" href="https://www.instagram.com/aadhrita2020/"><i class="fab fa-instagram fa-2x text-danger mx-6"></i></a>
+                <a target="_blank" href="https://twitter.com/aadhrita2020"><i class="fab fa-twitter fa-2x text-info mx-3"></i></a>
+                <a target="_blank" href="https://www.youtube.com/channel/UCF4345SfdBnl5R73kmIfICw"><i class="fab fa-youtube fa-2x text-danger mx-6"></i></a>
+              </div>
+            </div>    
+            <div class="col-md-4 text-white text-center text-md-left ">
+              <h4 class="my-3 text-warning">Contact details</h4>  
+              <div class="py-0 my-4">
+                <div>
+                  
+                </div>
+                <div>
+                  <ul>
+                    <li class="fa fa-phone  mx-auto my-3 "><a style="color:#50d8af;" href="tel:+917095847156">&nbsp Prasoon : +917095847156 </a></li>
+                    <!-- <li class="fa fa-phone  mx-auto my-3 "><a style="color:#50d8af;" href="tel:+919949110243">&nbsp Jeevan : +919949110243 </a></li> -->
+                    <li class="fa fa-envelope  mx-auto my-3 "><a style="color:#50d8af;" href="mailto:aadhrita2020@gmail.com">&nbsp aadhrita2020@gmail.com </a></li>
+                  </ul>
+                </div>
+              </div>
             </div>
-          </div>
-          <div class="col-md-3 text-white text-center text-md-left">
-            <div class="py-0 my-4" >
-              <div>
-                <p class="text-white text-center text-md-left"> <i class="fa fa-map-marker mx-auto  "></i>
+            <div class="col-md-4">
+              <h4 class="my-3 text-warning text-center text-md-left">Address & Map</h4>
+              <p class="text-white text-center text-md-left"> <i class="fa fa-map-marker mx-auto  "></i>
                     MVGR College of Engineering(A),
                     Vijayaram Nagar campus, Chintalavalasa,
-                    Vizianagaram, Andhra Pradesh 535005</p></i>
+                  Vizianagaram, Andhra Pradesh 535005</p></i>       
+              <div class="py-2">
+                <iframe width="230"
+                height="230"
+                frameborder="2" style="border:0;width:100%;height:auto;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3793.245677096944!2d83.40339111442094!3d18.06017798767875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a3be4c4081c8dbb%3A0x1c634bbcd67ea3bf!2sMaharaj%20Vijayaram%20Gajapathi%20Raj%20College%20of%20Engineering%20(Autonomous)!5e0!3m2!1sen!2sin!4v1577787304518!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
               </div>
-              <div>
-                  <p><i class="fa fa-phone  mx-auto my-3 "></i> Contact: 08922 241732,<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 08922 241039</p>
-              </div>
-  
-              <div>
-                  <p><i class="fa fa-envelope  mx-auto my-3"></i><a href="mailto:office@mvgrce.com">office@mvgrce.com</a></p>
-              </div>
-               <p class="footer-links font-weight-bold">
-                  <a class="text-white" href="#">Home |</a>
-                  <a class="text-white" href="#">Blog |</a>
-                  <a class="text-white" href="#">About |</a>
-                  <a class="text-white" href="#">Contact</a>
-              </p><br>
+            </div>
           </div>
         </div>
-        <div class="col-md-3 text-center">
-          <h5 class="my-3 text-white">Quicklinks</h5>
-          <ul class="text-warning my-2">
-            <li>Commities</li>
-            <li>Contact</li>
-            <li>Grievances</li>
-            <li>Directory</li>
-            <li>Downloads Section</li>
-          </ul>
-          <div class="py-1">
-            <a target="blank" href="https://www.facebook.com/pages/category/Event/Aadhritha-2020-108177254021540/"><i class="fab fa-facebook fa-2x text-primary mx-3"></i></a>
-            <a target="blank" href="https://www.instagram.com/aadhrita2020/"><i class="fab fa-instagram fa-2x text-danger mx-6"></i></a>
-            <a target="blank" href="https://twitter.com/MVGRCEA"><i class="fab fa-twitter fa-2x text-info mx-3"></i></a>
-            <a target="blank" href="https://www.youtube.com/results?search_query=mvgr+"><i class="fab fa-youtube fa-2x text-danger mx-6"></i></a>
-          </div>
+        <div class="foot" style="width: 100%;color: azure;background-color:dimgrey;height: 80px;margin-bottom: 0">
+          <p align="center" style="padding-top: 20px;">Copyright &copy;2020, AadhritaWebteam</p>
         </div>
-      </div>
-      </div>
-      <div class="foot" style="width: 100%;color: azure;background-color:dimgrey;height: 80px;margin-bottom: 0">
-        <p align="center" style="padding-top: 20px;">Copyright &copy;2020,MVGR</p>
-      </div>
-    </footer>
+      </footer>
     <script src="../../js/jquery.min.js"></script>
     <script src='https://kit.fontawesome.com/a076d05399.js'></script> 
     <script  src="../../js/common.js"></script>
     <script src="../../js/popper.min.js"></script>
+    <script type="text/javascript" src="../../js/navbar.js" ></script>
 	
 	<script>
 
